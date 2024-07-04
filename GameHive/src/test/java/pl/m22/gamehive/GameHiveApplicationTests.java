2abe74e7816e48c281b2b7cd918c961f7ba490c1
Minner22 <p_makuch@poczta.onet.pl>
2024-07-04 22:48:07 +0200

    Initial commit of backend part

---
+++ GameHive/src/test/java/pl/m22/gamehive/GameHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.m22.gamehive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
